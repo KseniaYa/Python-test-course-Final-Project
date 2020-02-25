1a3dcdbd09e8228992339554d7a7185d5f2e422f
Ksenia Yankovich <yankovich.k.s@gmail.com>
2020-02-25 14:28:41 +0100

    add implicit page transition

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-from pages.locators import MainPageLocators, LoginPageLocators
+from pages.locators import MainPageLocators
 
 
 
@@ -8,7 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     link = MainPageLocators.LINK
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_url()
 
 def test_guest_should_see_login_link(browser):
@@ -18,15 +19,19 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 def test_guest_should_see_login_form(browser):
-    link = LoginPageLocators.LINK
-    page = LoginPage(browser, link)
+    link = MainPageLocators.LINK
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_form()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
     
 def test_guest_should_see_register_form(browser):
-    link = LoginPageLocators.LINK
-    page = LoginPage(browser, link)
+    link = MainPageLocators.LINK
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_register_form()
+    page.go_to_login_page()
+    register_page = LoginPage(browser, browser.current_url)
+    register_page.should_be_register_form()
     
     
