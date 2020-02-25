9eae41c27997dce101557f3ad68d13de9f17d656
Ksenia Yankovich <yankovich.k.s@gmail.com>
2020-02-25 14:09:58 +0100

    add should see login/register form tests

--- pages/locators.py
+++ pages/locators.py
@@ -4,4 +4,8 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LINK = "http://selenium1py.pythonanywhere.com/"
 
+class LoginPageLocators():
+    LINK = "http://selenium1py.pythonanywhere.com/fr/accounts/login/"
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
     
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
 from pages.main_page import MainPage
-from pages.locators import MainPageLocators
+from pages.login_page import LoginPage
+from pages.locators import MainPageLocators, LoginPageLocators
+
+
 
 def test_guest_can_go_to_login_page(browser):
     link = MainPageLocators.LINK
@@ -12,3 +15,23 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_url(browser):
+    link = LoginPageLocators.LINK
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    
+def test_guest_should_see_login_form(browser):
+    link = LoginPageLocators.LINK
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+    
+def test_guest_should_see_register_form(browser):
+    link = LoginPageLocators.LINK
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+    
+    
