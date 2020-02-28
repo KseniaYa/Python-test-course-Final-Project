031af90c92716743a08048060697e3a8a5c4d543
Ksenia Yankovich <yankovich.k.s@gmail.com>
2020-02-28 16:12:07 +0100

    add mark login_guest

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,25 @@
+import pytest
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 from pages.locators import MainPageLocators
 
-
-def test_guest_can_go_to_login_page(browser):
-    link = MainPageLocators.LINK
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
-
-def test_guest_should_see_login_link(browser):
-    link = MainPageLocators.LINK
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = MainPageLocators.LINK
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_url()
+    
+    def test_guest_should_see_login_link(self, browser):
+        link = MainPageLocators.LINK
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_should_see_login_form(browser):
     link = MainPageLocators.LINK
